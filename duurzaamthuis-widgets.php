1e417ad8543747192e4b3a1c452c95a478e6d74c
Alex Ischenko <alex.ischenko@magnificsoft.com>
2021-06-21 16:38:47 +0300

    0.4.4

--- duurzaamthuis-widgets.php
+++ duurzaamthuis-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Duurzaamthuis Widgets
  * Description: Custom Elementor widgets
  * Plugin URI:  https://magnificsoft.com/
- * Version:     0.4.3
+ * Version:     0.4.4
  * Author:      Alex Ischenko
  * Text Domain: duurzaamthuis
  */
--- widgets/page-header.php
+++ widgets/page-header.php
@@ -131,6 +131,7 @@ class DH_Page_Header extends \Elementor\Widget_Base {
       $tags = wp_get_post_terms( $post_id, 'post_tag', array('fields' => 'ids') );
 
       ?>
+         <!-- Impact section -->
          <div class="elementor-section elementor-section-boxed" style="background-image: url(<?php echo $featured_image_url; ?>);">
 
             <div class="dh-overlay"></div>
@@ -174,56 +175,79 @@ class DH_Page_Header extends \Elementor\Widget_Base {
                      $gemak = $gemak . ' uur';
                   } 
 
+                  $vervuiling = get_post_meta( $post_id, 'vervuiling', true );
+                  
+                  $advies = get_post_meta( $post_id, 'advies', true );
+
                   $subsidie = get_post_meta( $post_id, 'subsidie', true );
+
                   $calculations_text = get_post_meta( $post_id, 'calculations-text', true );
+
+                  $intro_text = get_post_meta( $post_id, 'intro-text', true );
                ?>
                   
                <?php if ( $settings['show_infobar'] && ( $milieuwinst || $prijs || $terugverdientijd || $gemak || $subsidie ) ) : ?>
                   <div class="dh-page-header-impact">
-                     <div class="dh-page-header-features">
+                     <div class="dh-page-header-features-section">
                         <div class="dh-page-header-features-title">Impact</div>
-                        <?php if ( $milieuwinst ) { ?>
-                           <div class="dh-page-header-feature">
-                              <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-benefit"></i>Milieuwinst</div>
-                              <div class="dh-page-header-feature-value milieuwinst"><?php echo $milieuwinst; ?></div>
-                           </div>
-                        <?php } ?>
+                        <div class="dh-page-header-features">
+                           <?php if ( $milieuwinst ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-benefit"></i>Milieuwinst</div>
+                                 <div class="dh-page-header-feature-value milieuwinst"><?php echo $milieuwinst; ?></div>
+                              </div>
+                           <?php } ?>
 
-                        <?php if ( $prijs ) { ?>
-                           <div class="dh-page-header-feature">
-                              <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-price"></i>Kosten</div>
-                              <div class="dh-page-header-feature-value"><?php echo $prijs; ?></div>
-                           </div>
-                        <?php } ?>
+                           <?php if ( $prijs ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-price"></i>Kosten</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $prijs; ?></div>
+                              </div>
+                           <?php } ?>
 
-                        <?php if ( $terugverdientijd ) { ?>
-                           <div class="dh-page-header-feature">
-                              <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-payback"></i>Terugverdientijd</div>
-                              <div class="dh-page-header-feature-value"><?php echo $terugverdientijd; ?></div>
-                           </div>
-                        <?php } ?>
+                           <?php if ( $terugverdientijd ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-payback"></i>Terugverdientijd</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $terugverdientijd; ?></div>
+                              </div>
+                           <?php } ?>
 
-                        <?php if ( $gemak ) { ?>
-                           <div class="dh-page-header-feature">
-                              <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-convenience"></i>Gemak</div>
-                              <div class="dh-page-header-feature-value"><?php echo $gemak; ?></div>
-                           </div>
-                        <?php } ?>
+                           <?php if ( $gemak ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-convenience"></i>Gemak</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $gemak; ?></div>
+                              </div>
+                           <?php } ?>
 
-                        <?php if ( $subsidie ) { ?>
-                           <div class="dh-page-header-feature">
-                              <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-subsidy"></i>Subsidie</div>
-                              <div class="dh-page-header-feature-value"><?php echo $subsidie; ?></div>
-                           </div>
-                        <?php } ?>
+                           <?php if ( $vervuiling ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-convenience"></i>Vervuiling</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $vervuiling; ?></div>
+                              </div>
+                           <?php } ?>
+
+                           <?php if ( $advies ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-convenience"></i>Advies</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $advies; ?></div>
+                              </div>
+                           <?php } ?>
+
+                           <?php if ( $subsidie ) { ?>
+                              <div class="dh-page-header-feature">
+                                 <div class="dh-page-header-feature-title"><i class="dh-icon dh-icon-subsidy"></i>Subsidie</div>
+                                 <div class="dh-page-header-feature-value"><?php echo $subsidie; ?></div>
+                              </div>
+                           <?php } ?>
+                        </div>
                      </div>
 
 
                      <?php if ( $calculations_text ) { ?>
                         <div class="dh-page-header-calc">
                            <div class="dh-page-header-calc-toggle">
-                              <div class="close">Close Calculation<i class="dh-icon dh-icon-arrow-up"></i></div>
-                              <div class="open">Open Calculation<i class="dh-icon dh-icon-arrow-down"></i></div>
+                              <div class="close">Sluiten<i class="dh-icon dh-icon-arrow-up"></i></div>
+                              <div class="open">Toelichting<i class="dh-icon dh-icon-arrow-down"></i></div>
                            </div>
                            <div class="dh-page-header-calc-text"><?php echo $calculations_text; ?></div>
                         </div>
@@ -260,9 +284,180 @@ class DH_Page_Header extends \Elementor\Widget_Base {
 
          </div>
 
+
+         <!-- Intro section -->
+         <?php if ( $intro_text ) { ?>
+            <div class="elementor-section elementor-section-boxed">
+               <div class="dh-page-intro elementor-container">
+                  <?php echo $intro_text; ?>
+               </div>
+            </div>
+         <?php } ?>
+
       <?php
 
 
 	}
 	
 }
+
+
+/**
+ * 
+ * class for adding Impact meta section to Elementor page/post settings
+ * 
+ */
+class DH_Impact_Fields {
+	public function __construct() {
+		add_action( 'elementor/element/wp-post/document_settings/after_section_end', [ $this, 'add_post_settings_controls' ] );
+		add_action( 'elementor/element/wp-page/document_settings/after_section_end', [ $this, 'add_page_settings_controls' ] );
+		add_action( 'elementor/document/after_save', [ $this, 'save_settings' ], 10, 2 );
+	}
+	public function add_post_settings_controls( \Elementor\Core\DocumentTypes\Post $post ) {
+		$this->add_controls( $post );
+	}
+	public function add_page_settings_controls( \Elementor\Core\DocumentTypes\Page $page ) {
+		$this->add_controls( $page );
+	}
+	public function add_controls( $post ) {
+		$post->start_controls_section( 'section_impact', [
+			'label' => __( 'Impact', 'duurzaamthuis' ),
+			'tab' => \Elementor\Controls_Manager::TAB_SETTINGS, // https://developers.elementor.com/elementor-element-panel-tabs/
+		]);
+			$post->add_control(
+				'milieuwinst',
+				[
+					'label' => __( 'Milieuwinst', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+					// 'default' => '-',
+				]
+			);
+			$post->add_control(
+				'prijs',
+				[
+					'label' => __( 'Prijs', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+					// 'default' => '-',
+				]
+			);
+			$post->add_control(
+				'terugverdientijd',
+				[
+					'label' => __( 'Terugverdientijd', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+				]
+			);
+			$post->add_control(
+				'gemak',
+				[
+					'label' => __( 'Gemak', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+				]
+			);
+			$post->add_control(
+				'subsidie',
+				[
+					'label' => __( 'Subsidie', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+				]
+			);
+			$post->add_control(
+				'vervuiling',
+				[
+					'label' => __( 'Vervuilinge', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+				]
+			);
+			$post->add_control(
+				'advies',
+				[
+					'label' => __( 'Advies', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::TEXT,
+				]
+			);
+			$post->add_control(
+				'calculations_text',
+				[
+					'label' => __( 'Calculations', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::WYSIWYG,
+				]
+			);
+			$post->add_control(
+				'impact_button',
+				[
+					'type' => \Elementor\Controls_Manager::RAW_HTML,
+					'raw' => '<button class="elementor-update-preview-button elementor-button elementor-button-success" onclick="elementor.saver.update.apply().then(function(){elementor.reloadPreview();});">Update And Reload Preview</button>',
+				]
+			);
+		$post->end_controls_section(); 
+	}
+	public function save_settings( $instance, $data ) {
+		$post_id = $instance->get_post()->ID;
+
+		if ( empty( $data) ) return;
+
+		$settings = $data['settings'];
+
+		update_post_meta( $post_id, 'milieuwinst', $settings['milieuwinst'] ?? '' );
+		update_post_meta( $post_id, 'prijs', $settings['prijs'] ?? '' );
+		update_post_meta( $post_id, 'terugverdientijd', $settings['terugverdientijd'] ?? '' );
+		update_post_meta( $post_id, 'gemak', $settings['gemak'] ?? '' );
+		update_post_meta( $post_id, 'subsidie', $settings['subsidie'] ?? '' );
+		update_post_meta( $post_id, 'vervuiling', $settings['vervuiling'] ?? '' );
+		update_post_meta( $post_id, 'advies', $settings['advies'] ?? '' );
+		update_post_meta( $post_id, 'calculations-text', $settings['calculations_text'] ?? '' );
+	}
+}
+new DH_Impact_Fields();
+
+
+/**
+ * 
+ * class for adding Impact meta section to Elementor page/post settings
+ * 
+ */
+class DH_Intro_Field {
+	public function __construct() {
+		add_action( 'elementor/element/wp-post/document_settings/after_section_end', [ $this, 'add_post_settings_controls' ] );
+		add_action( 'elementor/element/wp-page/document_settings/after_section_end', [ $this, 'add_page_settings_controls' ] );
+		add_action( 'elementor/document/after_save', [ $this, 'save_settings' ], 10, 2 );
+	}
+	public function add_post_settings_controls( \Elementor\Core\DocumentTypes\Post $post ) {
+		$this->add_controls( $post );
+	}
+	public function add_page_settings_controls( \Elementor\Core\DocumentTypes\Page $page ) {
+		$this->add_controls( $page );
+	}
+	public function add_controls( $post ) {
+		$post->start_controls_section( 'section_intro', [
+			'label' => __( 'Introduction', 'duurzaamthuis' ),
+			'tab' => \Elementor\Controls_Manager::TAB_SETTINGS, // https://developers.elementor.com/elementor-element-panel-tabs/
+		]);
+			$post->add_control(
+				'intro_text',
+				[
+					'label' => __( 'Introduction Text', 'duurzaamthuis' ),
+					'type' => \Elementor\Controls_Manager::WYSIWYG,
+				]
+			);
+			$post->add_control(
+				'intro_button',
+				[
+					'type' => \Elementor\Controls_Manager::RAW_HTML,
+					'raw' => '<button class="elementor-update-preview-button elementor-button elementor-button-success" onclick="elementor.saver.update.apply().then(function(){elementor.reloadPreview();});">Update And Reload Preview</button>',
+				]
+			);
+		$post->end_controls_section(); 
+	}
+	public function save_settings( $instance, $data ) {
+		$post_id = $instance->get_post()->ID;
+
+		if ( empty( $data) ) return;
+
+		$settings = $data['settings'];
+
+		update_post_meta( $post_id, 'intro-text', $settings['intro_text'] ?? '' );
+
+	}
+}
+new DH_Intro_Field();
