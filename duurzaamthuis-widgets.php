8eb24e6ee12195f98c0ae7cbc7bcb8d6910eff20
Alex Ischenko <alex.ischenko@magnificsoft.com>
2021-07-25 14:40:08 +0300

    0.6.17

--- duurzaamthuis-widgets.php
+++ duurzaamthuis-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Duurzaamthuis Widgets
  * Description: Custom Elementor widgets
  * Plugin URI:  https://magnificsoft.com/
- * Version:     0.6.16
+ * Version:     0.6.17
  * Author:      Alex Ischenko
  * Text Domain: duurzaamthuis
  */
--- widgets/mega-menu.php
+++ widgets/mega-menu.php
@@ -240,106 +240,109 @@ class DH_Mega_Menu extends \Elementor\Widget_Base {
 		}
 
 		$items = wp_get_nav_menu_items( $settings['menu'] );
-		$items_tree = $this->build_menu_tree( $items );      
+		if ( $items ) {
+			$items_tree = $this->build_menu_tree( $items );      
 
-		$mobile_items = wp_get_nav_menu_items( $settings['mobile_menu'] ?: $settings['menu'] );
-		$mobile_items_tree = $this->build_menu_tree( $mobile_items );      
-
-		echo '<nav><ul class="dh-menu">';
-			foreach ( $items_tree as $top_level_item ) {
-				echo '<li class="dh-menu-item dh-menu-top-item menu-item menu-item-' . $top_level_item->ID . ' ' . implode( " ", $top_level_item->classes ) . '">';
-					echo '<a href="' . $top_level_item->url . '" class="">';
-						$icon_classes = get_post_meta( $top_level_item->ID, '_menu_item_icon_classes', true );
-						if ( $icon_classes ) {
-							echo '<i class="' . $icon_classes . '"></i>';
-						}
-						echo $top_level_item->post_title;
-						if ( ! empty( $top_level_item->children ) ) {
-							echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-down"></i>';
-						}
-					echo '</a>';
-
-					// dropdown
-					if ( ! empty( $top_level_item->children ) ) {
-
-						echo '<ul class="dh-megamenu elementor-section elementor-section-boxed">';
-							echo '<div class="elementor-container">';
-								foreach ( $top_level_item->children as $second_level_item ) {
-									echo '<li class="dh-menu-item dh-second-level-item menu-item menu-item-' . $second_level_item->ID . ' ' . implode( " ", $second_level_item->classes ) . '">';
-										echo '<a href="' . ( $second_level_item->url != '#' ? $second_level_item->url : '' ) . '" class="dh-menu-second-level-heading">' . $second_level_item->post_title . '</a>';
-										echo '<ul class="dh-megamenu-second-level-item">';
-											if ( ! empty( $second_level_item->children ) ) {
-												foreach ( $second_level_item->children as $third_level_item ) {
-													echo '<li class="dh-menu-item dh-third-level-item menu-item menu-item-' . $third_level_item->ID . ' ' . implode( " ", $third_level_item->classes ) . '">';
-														echo '<a href="' . $third_level_item->url . '" class="dh-menu-third-item">' . $third_level_item->post_title . '</a>';
-													echo '</li>';
-												}
-											}
-										echo '</ul>';
-										if ( $second_level_item->url != '#' ) {
-											echo '<a href="' . $second_level_item->url . '" class="dh-menu-second-level-more">Lees Verder <i class="dh-icon dh-icon-arrow-right"></i></a>';
-										}
-									echo '</li>';
-								}
-						echo '</div>';
-						echo '</ul>';
-
-					}
-				echo '</li>';
-			}
-		echo '</ul></nav>';  
-
-
-		echo '<div class="dh-mobile-menu-toggle"><i class="dh-icon dh-icon-menu"></i></div>';
-		echo '<nav><ul class="dh-mobile-menu">';
-			echo '<div class="dh-mobile-menu-wrapper">';
-
-				foreach ( $mobile_items_tree as $top_level_item ) {
+			echo '<nav><ul class="dh-menu">';
+				foreach ( $items_tree as $top_level_item ) {
 					echo '<li class="dh-menu-item dh-menu-top-item menu-item menu-item-' . $top_level_item->ID . ' ' . implode( " ", $top_level_item->classes ) . '">';
-						$icon_classes = get_post_meta( $top_level_item->ID, '_menu_item_icon_classes', true );
-						if ( empty( $top_level_item->children ) ) {
-							echo '<a class="dh-mobile-submenu-toggle">';
-								if ( $icon_classes ) {
-									echo '<i class="' . $icon_classes . '"></i>';
-								}
-								echo $top_level_item->post_title;
-							echo '</a>';
-						} else {
-							echo '<a class="dh-mobile-submenu-toggle">';
-								if ( $icon_classes ) {
-									echo '<i class="' . $icon_classes . '"></i>';
-								}
-								echo $top_level_item->post_title;
-								echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-right"></i>';
-							echo '</a>';
-							echo '<ul class="dh-submenu">';
-								foreach ( $top_level_item->children as $second_level_item ) {
-									echo '<li class="dh-menu-item dh-second-level-item menu-item menu-item-' . $second_level_item->ID . ' ' . implode( " ", $second_level_item->classes ) . '">';
-										if ( empty( $second_level_item->children ) ) {
-											echo '<a href="' . $second_level_item->url . '">' . $second_level_item->post_title . '</a>';
-										} else {
-											echo '<a class="dh-mobile-submenu-toggle">';
-												echo $second_level_item->post_title;
-												echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-right"></i>';
-											echo '</a>';
-											echo '<ul class="dh-submenu">';
-												foreach ( $second_level_item->children as $third_level_item ) {
-													echo '<li class="dh-menu-item dh-third-level-item menu-item menu-item-' . $third_level_item->ID . ' ' . implode( " ", $third_level_item->classes ) . '">';
-														echo '<a href="' . $third_level_item->url . '">' . $third_level_item->post_title . '</a>';
-													echo '</li>';
+						echo '<a href="' . $top_level_item->url . '" class="">';
+							$icon_classes = get_post_meta( $top_level_item->ID, '_menu_item_icon_classes', true );
+							if ( $icon_classes ) {
+								echo '<i class="' . $icon_classes . '"></i>';
+							}
+							echo $top_level_item->post_title;
+							if ( ! empty( $top_level_item->children ) ) {
+								echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-down"></i>';
+							}
+						echo '</a>';
+
+						// dropdown
+						if ( ! empty( $top_level_item->children ) ) {
+
+							echo '<ul class="dh-megamenu elementor-section elementor-section-boxed">';
+								echo '<div class="elementor-container">';
+									foreach ( $top_level_item->children as $second_level_item ) {
+										echo '<li class="dh-menu-item dh-second-level-item menu-item menu-item-' . $second_level_item->ID . ' ' . implode( " ", $second_level_item->classes ) . '">';
+											echo '<a href="' . ( $second_level_item->url != '#' ? $second_level_item->url : '' ) . '" class="dh-menu-second-level-heading">' . $second_level_item->post_title . '</a>';
+											echo '<ul class="dh-megamenu-second-level-item">';
+												if ( ! empty( $second_level_item->children ) ) {
+													foreach ( $second_level_item->children as $third_level_item ) {
+														echo '<li class="dh-menu-item dh-third-level-item menu-item menu-item-' . $third_level_item->ID . ' ' . implode( " ", $third_level_item->classes ) . '">';
+															echo '<a href="' . $third_level_item->url . '" class="dh-menu-third-item">' . $third_level_item->post_title . '</a>';
+														echo '</li>';
+													}
 												}
 											echo '</ul>';
-										}
-									echo '</li>';
-								}
-
+											if ( $second_level_item->url != '#' ) {
+												echo '<a href="' . $second_level_item->url . '" class="dh-menu-second-level-more">Lees Verder <i class="dh-icon dh-icon-arrow-right"></i></a>';
+											}
+										echo '</li>';
+									}
+							echo '</div>';
 							echo '</ul>';
+
 						}
 					echo '</li>';
 				}
+			echo '</ul></nav>';  
+		}
 
-			echo '</div>';  
-		echo '</ul></nav>';  
+		$mobile_items = wp_get_nav_menu_items( $settings['mobile_menu'] ?: $settings['menu'] );
+		if ( $mobile_items ) {
+			$mobile_items_tree = $this->build_menu_tree( $mobile_items );      
+
+			echo '<div class="dh-mobile-menu-toggle"><i class="dh-icon dh-icon-menu"></i></div>';
+			echo '<nav><ul class="dh-mobile-menu">';
+				echo '<div class="dh-mobile-menu-wrapper">';
+
+					foreach ( $mobile_items_tree as $top_level_item ) {
+						echo '<li class="dh-menu-item dh-menu-top-item menu-item menu-item-' . $top_level_item->ID . ' ' . implode( " ", $top_level_item->classes ) . '">';
+							$icon_classes = get_post_meta( $top_level_item->ID, '_menu_item_icon_classes', true );
+							if ( empty( $top_level_item->children ) ) {
+								echo '<a class="dh-mobile-submenu-toggle">';
+									if ( $icon_classes ) {
+										echo '<i class="' . $icon_classes . '"></i>';
+									}
+									echo $top_level_item->post_title;
+								echo '</a>';
+							} else {
+								echo '<a class="dh-mobile-submenu-toggle">';
+									if ( $icon_classes ) {
+										echo '<i class="' . $icon_classes . '"></i>';
+									}
+									echo $top_level_item->post_title;
+									echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-right"></i>';
+								echo '</a>';
+								echo '<ul class="dh-submenu">';
+									foreach ( $top_level_item->children as $second_level_item ) {
+										echo '<li class="dh-menu-item dh-second-level-item menu-item menu-item-' . $second_level_item->ID . ' ' . implode( " ", $second_level_item->classes ) . '">';
+											if ( empty( $second_level_item->children ) ) {
+												echo '<a href="' . $second_level_item->url . '">' . $second_level_item->post_title . '</a>';
+											} else {
+												echo '<a class="dh-mobile-submenu-toggle">';
+													echo $second_level_item->post_title;
+													echo '<i class="dh-dropdown-indicator dh-icon dh-icon-arrow-right"></i>';
+												echo '</a>';
+												echo '<ul class="dh-submenu">';
+													foreach ( $second_level_item->children as $third_level_item ) {
+														echo '<li class="dh-menu-item dh-third-level-item menu-item menu-item-' . $third_level_item->ID . ' ' . implode( " ", $third_level_item->classes ) . '">';
+															echo '<a href="' . $third_level_item->url . '">' . $third_level_item->post_title . '</a>';
+														echo '</li>';
+													}
+												echo '</ul>';
+											}
+										echo '</li>';
+									}
+
+								echo '</ul>';
+							}
+						echo '</li>';
+					}
+
+				echo '</div>';  
+			echo '</ul></nav>';  
+		}
 		
 		
 		// mobile
