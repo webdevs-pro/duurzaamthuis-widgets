d128938fd8657b9ab3895670f9fa9fd7de92b566
Alex Ischenko <alex.ischenko@magnificsoft.com>
2021-11-08 12:16:16 +0200

    0.9.4

--- duurzaamthuis-widgets.php
+++ duurzaamthuis-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Duurzaamthuis Widgets
  * Description: Custom Elementor widgets
  * Plugin URI:  https://magnificsoft.com/
- * Version:     0.9.3
+ * Version:     0.9.4
  * Author:      Alex Ischenko
  * Text Domain: duurzaamthuis
  */
