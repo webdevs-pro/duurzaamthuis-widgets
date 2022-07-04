f56449a5ac65e1ddc93a372ff7520211b061c994
WebDevs.pro <shock_31337@ukr.net>
2022-07-04 18:03:37 +0200

    1.88

--- duurzaamthuis-widgets.php
+++ duurzaamthuis-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Duurzaamthuis Widgets
  * Description: Custom Elementor widgets
  * Plugin URI:  https://magnificsoft.com/
- * Version:     1.87
+ * Version:     1.88
  * Author:      Alex Ischenko
  * Text Domain: duurzaamthuis
  */
